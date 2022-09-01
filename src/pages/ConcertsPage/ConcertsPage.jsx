40687651c9284ce438a08306d98d1274e3a7c80f
Gabriella Hevesy <55456609+napoligab@users.noreply.github.com>
2022-09-01 17:09:21 +0100

    searchbar and edit concert

--- src/pages/ConcertsPage/ConcertsPage.jsx
+++ src/pages/ConcertsPage/ConcertsPage.jsx
@@ -9,6 +9,23 @@ import axios from 'axios';
 function ConcertsPage() {
   const { user } = useContext(AuthContext);
   const [concerts, setConcerts] = useState([]);
+  const [query, setQuery] = useState('');
+
+  //fazer um loop nos concerts e filtrar os que dão match no nome do artista
+
+  /* const handleInput = (e) => setInput(e.target.value); */
+
+  const handleQuery = (e) => {
+    setQuery(e.target.value);
+    handleSearch(e.target.value);
+  };
+  const handleSearch = (searchStr) => {
+    let filtered = concerts.filter((el) => {
+      return el.artist.toLowerCase().includes(searchStr.toLowerCase());
+    });
+
+    setConcerts(filtered);
+  };
 
   const getConcerts = async () => {
     try {
@@ -36,12 +53,18 @@ function ConcertsPage() {
     <div>
       <h2>concerts</h2>
 
-      <Searchbar />
+      <input
+        type="text"
+        className="text-black"
+        onChange={handleQuery}
+        value={query}
+      />
 
-      {concerts.map((concert) => {
-        return (
-          <div className="concert-card mt-4" key={concert._id}>
-            <Link to={`/concerts/${concert._id}`}>
+      {user &&
+        concerts &&
+        concerts.map((concert) => {
+          return (
+            <div className="concert-card mt-4" key={concert._id}>
               <h3>{concert.artist}</h3>
               <img className="artist-pic" src={concert.image} alt="artist" />
               <h4>
@@ -49,36 +72,34 @@ function ConcertsPage() {
               </h4>
               <h4>{concert.city}</h4>
               <h4>{concert.venue}</h4>
-              {concert.budget <= 0 ?
-                <h4>this concert is happening! </h4> :
+              {concert.budget <= 0 ? (
+                <h4>this concert is happening! </h4>
+              ) : (
                 <h4>{concert.budget}</h4>
-              }
+              )}
               <h4>{concert.minTicket}€</h4>
               <h4>{concert.usersFunding.length}</h4>
-            </Link>
 
+              {!user.admin && (
+                <Link to={`/concerts/${concert._id}/fund`}>
+                  <button className="fund-btn">fund!</button>
+                </Link>
+              )}
 
-            {!user.admin &&
-            <Link to={`/concerts/${concert._id}/fund`}>
-             <button className="fund-btn">fund!</button>
-            </Link>} 
+              {user.admin && (
+                <Link to={`/concerts/${concert._id}/edit`}>
+                  <button className="fund-btn">edit concert!</button>
+                </Link>
+              )}
 
-           {user.admin && 
-            <Link to={`/concerts/${concert._id}/edit`}>
-             <button className="fund-btn">edit concert!</button>
-            </Link>
-            } 
-
-            {user.admin &&
-           <Link to={'/createconcerts'}>
-            <button className="fund-btn">create concert!</button>
-           </Link>}
-
-  
-          </div> 
-
-        );
-      })}
+              {user.admin && (
+                <Link to={'/createconcerts'}>
+                  <button className="fund-btn">create concert!</button>
+                </Link>
+              )}
+            </div>
+          );
+        })}
     </div>
   );
 }
