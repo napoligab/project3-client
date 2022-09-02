3b2f1d5b5457530bc9dd8063d3cf2464e0f6fb80
Gabriella Hevesy <55456609+napoligab@users.noreply.github.com>
2022-09-02 10:18:48 +0100

    text area comment fixed

--- src/components/Request/Request.jsx
+++ src/components/Request/Request.jsx
@@ -1,22 +1,22 @@
-import {useState, useContext} from 'react';
-import {AuthContext} from '../../context/auth.context';
-import {useNavigate} from 'react-router-dom';
+import { useState, useContext } from 'react';
+import { AuthContext } from '../../context/auth.context';
+import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
 
 function Request() {
-const [message, setMessage] = useState('');
-const {user} = useContext(AuthContext);
+  const [message, setMessage] = useState('');
+  const { user } = useContext(AuthContext);
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const handleMessage = (e) => setMessage(e.target.value)
+  const handleMessage = (e) => setMessage(e.target.value);
 
-const handleSubmit = (e) => {
+  const handleSubmit = (e) => {
     e.preventDefault();
 
     const token = localStorage.getItem('authToken');
 
-    const body = {author: user._id, message} ;
+    const body = { author: user._id, message };
 
     axios
       .post(`${process.env.REACT_APP_API_URL}/api/request`, body, {
@@ -33,17 +33,21 @@ const handleSubmit = (e) => {
     setMessage('');
   };
 
-
   return (
     <div>
-
-    <form onSubmit={handleSubmit}>
-    <textarea className="message text-black textarea textarea-secondary" type="text" rows='15' cols='40' onChange={handleMessage} placeholder="who would you like to see in your city?">
-    </textarea>
-    <button className="btn btn-primary btn-position">send request</button>
-   </form>
+      <form onSubmit={handleSubmit}>
+        <textarea
+          className="message text-WHITE textarea textarea-secondary"
+          type="text"
+          rows="15"
+          cols="40"
+          onChange={handleMessage}
+          placeholder="who would you like to see in your city?"
+        ></textarea>
+        <button className="btn btn-primary btn-position">send request</button>
+      </form>
     </div>
-  )
+  );
 }
 
-export default Request
+export default Request;
